20791f3f960d73836bcb5236433658e93bd263a3
baqaB4731 <baqa4731@gmail.com>
2024-11-01 23:51:22 +0330

    after edit sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,9 +9,9 @@ const Sidebar: React.FC<SidebarProps> = ({ className, isRtl }) => {
     return (
         <div className={`${className} ${isRtl ? 'sidebar-right' : 'sidebar-left'}`}>
             <ul className='menu'>
-                <li className='btn'>Dashboard</li>
-                <li className='btn'>Tasks</li>
-                <li className='btn'>Calendar</li>
+                <li className='btn2'>Dashboard</li>
+                <li className='btn2'>Tasks</li>
+                <li className='btn2'>Calendar</li>
             </ul>
         </div>
     );
