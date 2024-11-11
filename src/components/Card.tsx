fddfa11efde2d08d056140976c563a05f8f3b526
baqaB4731 <baqa4731@gmail.com>
2024-11-11 22:53:18 +0330

    edit card component  - is done style and state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface CardProps {
     isSidebarRight: boolean;
     title: string;
     description: string;
-  }
+}
 
 const Card: React.FC<CardProps> = ({ isSidebarRight, title, description }) => {
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    const handleCompleteTask = () => {
+        setIsCompleted(!isCompleted);
+    };
+
     return (
-        <div className={`card ${isSidebarRight ? "rtl-mode" : "ltr-mode "}`}>
-            <h2>{title}</h2>
+        <div className={`card ${isCompleted ? "completed" :""}`}>
+            <h2 className={`${isSidebarRight ? "rtl-mode" : "ltr-mode "}`}>{title}</h2>
             <p>{description}</p>
+            <button className={`completedBtn ${isCompleted ? "completedBtnDone" :""}`} onClick={handleCompleteTask}>
+                {isCompleted ? "Mark as Incomplete" : "Mark as Completed"}
+            </button>
         </div>
     )
 }
+
 
 export default Card;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -12,7 +12,7 @@ const Dashboard: React.FC = () => {
     const [isSidebarRight, setIsSidebarRight] = useState(false);
 
     const cardsDataObject: CardData[] = [
-        { id: 1, title: 'Card 1', description: 'This is the first card' },
+        { id: 1, title: 'Card 1', description: 'This is the first card ' },
         { id: 2, title: 'Card 2', description: 'This is the second card' },
         { id: 3, title: 'Card 3', description: 'This is the third card' },
       ];
